1f4e9e154e4d9da44a87edb1a750975a8ab6d905
Mohanee <mohanee99@gmail.com>
2020-09-29 17:07:40 +0530

    [mohanee] add master

---
+++ EmpWageProblem23/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageProblem23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome to Employee Wage Computation");
+        }
+    }
+}
